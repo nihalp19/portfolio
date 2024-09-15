50c6615841f8765ec882bf2f0f38dc4c9e551fb6
nihalp19 <nihalpanday2020@gmail.com>
2024-09-15 16:45:52 +0530

    add home-page navbar hero-section

---
+++ portfolio/src/components/Hero.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { HERO } from "../constants/index"
+import carImg from "../assets/carl.jpeg"
+
+function Hero() {
+  return (
+    <section className='flex min-h-screen flex-wrap items-center'>
+      <div className='w-full md:w-1/2'>
+        <h2 className='my-8 p-2 text-4xl font-bold md:text-5xl lg:text-[7rem]'>
+          {HERO.name}
+        </h2>
+        <p className='p-2 text-3xl tracking-tighter lg:text-4xl'>{HERO.greet}</p>
+        <p className='mb-8 p-2 text-xl'>
+          {HERO.description}
+        </p>
+      </div>
+      <div className='w-full md:w-1/2 lg:p-8'>
+        <div className='flex justify-center'>
+          <img src={carImg} width={550} height={550} alt='Nihal Panday'  className='rounded-3xl'/>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
