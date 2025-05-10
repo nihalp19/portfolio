dbaa2140fab87303fb5d112fbda7a4284e3d4225
Nihal Panday <nihalpanday2020@gmail.com>
2025-05-10 19:51:59 +0530

    updated app.jsx

---
+++ frontend/src/hooks/useMousePosition .jsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+
+export const useMousePosition = () => {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const mouseMove = (e) => {
+      setMousePosition({
+        x: e.clientX,
+        y: e.clientY
+      });
+    };
+
+    window.addEventListener("mousemove", mouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", mouseMove);
+    };
+  }, []);
+
+  return mousePosition;
+};
