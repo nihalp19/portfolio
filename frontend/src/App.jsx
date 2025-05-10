f64e689198fa8f6f003802f6360ca864d019d829
Nihal Panday <nihalpanday2020@gmail.com>
2025-05-10 19:06:17 +0530

    updated resume

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -259,7 +259,7 @@ function App() {
                 transition={{ duration: 0.6, delay: 0.6 }}
               >
                 <motion.a
-                  href="hhttps://drive.google.com/file/d/1j9cvyB2bPovhN_JBZ3ZL2ainngBs1oPB/view?usp=sharing"
+                  href="https://drive.google.com/file/d/1j9cvyB2bPovhN_JBZ3ZL2ainngBs1oPB/view?usp=sharing"
                   target="_blank"
                   className="inline-flex items-center gap-2 px-6 py-3 bg-white text-black rounded-lg font-medium hover:bg-neutral-200 transition-colors"
                   whileHover={{ y: -2 }}
